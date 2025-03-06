127a0f2643fb5d680d2af4be4818730a7211de57
Vanessa Valladares <100142668+vivq22@users.noreply.github.com>
2025-03-05 18:56:03 -0600

    feat: HTML index

--- src/main.js
+++ src/main.js
@@ -1,24 +1,65 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
 
-setupCounter(document.querySelector('#counter'))
+
+const audio = document.createElement("audio");
+audio.preload = "auto";
+audio.src = "img/mp3/Strawberry.mp3";
+audio.autoplay = true;
+audio.loop = true;
+audio.volume = 0.3;
+
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const phone = document.getElementById('phone').value.trim();
+    const subject = document.getElementById('subject').value.trim();
+    const message = document.getElementById('message').value.trim();
+
+    if (name === "") {
+        alert("Por favor, ingresa tu nombre completo.");
+        return;
+    }
+
+    if (email === "") {
+        alert("Por favor, ingresa tu correo electrónico.");
+        return;
+    }
+
+    const emailPattern = /^[^@]+@[^@]+\.[a-zA-Z]{2,6}$/;
+    if (!emailPattern.test(email)) {
+        alert("Por favor, ingresa un correo electrónico válido.");
+        return;
+    }
+
+    if (phone === "") {
+        alert("Por favor, ingresa tu número de teléfono.");
+        return;
+    }
+
+    const phonePattern = /^[0-9]{10}$/;
+    if (!phonePattern.test(phone)) {
+        alert("Por favor, ingresa un número de teléfono válido con 10 dígitos.");
+        return;
+    }
+
+    if (subject === "") {
+        alert("Por favor, ingresa un asunto.");
+        return;
+    }
+
+    if (message === "") {
+        alert("Por favor, ingresa tu mensaje.");
+        return;
+    }
+
+    alert("¡Formulario enviado correctamente!");
+
+});
